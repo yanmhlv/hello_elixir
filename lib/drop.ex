3d428f790aa3f495feb94958e3f532f8d2c1af0b
Yan Mihailov <yanmhlv@gmail.com>
2017-11-16 11:59:44 +0300

    example 2.10: added @doc to Drop module

--- lib/drop.ex
+++ lib/drop.ex
@@ -1,4 +1,8 @@
 defmodule Drop do
+    @doc """
+    вычисляет скорость свободного падающего объекта на Земле,
+    как если бы он падал в вакууме
+    """
     def fall_velocity(distance, gravity \\ 9.8) do
         import :math, only: [sqrt: 1]
         sqrt(2 * gravity * distance)
