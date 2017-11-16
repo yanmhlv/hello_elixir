3ea8c3cca93474a9f38eb675806b2e57f030e2fc
Yan Mihailov <yanmhlv@gmail.com>
2017-11-16 12:06:37 +0300

    example 3.2: added limiter to Drop.fall_velocity funcs

--- lib/drop.ex
+++ lib/drop.ex
@@ -1,13 +1,13 @@
 defmodule Drop do   
-    def fall_velocity(:earth, distance) do
+    def fall_velocity(:earth, distance) when distance >= 0do
         :math.sqrt(2 * 9.8 * distance)
     end
 
-    def fall_velocity(:moon, distance) do
+    def fall_velocity(:moon, distance) when distance >= 0do
         :math.sqrt(2 * 1.6 * distance)
     end
 
-    def fall_velocity(:marc, distance) do
+    def fall_velocity(:marc, distance) when distance >= 0do
         :math.sqrt(2 * 3.71 * distance)
     end
 end
